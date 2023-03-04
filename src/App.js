03f6b32dfb1c39fbb41a1e54f61a9e48463aa6ea
Jake Derhalli <45342768+JuiceboxJones@users.noreply.github.com>
2023-03-04 02:25:02 +0000

    Do not visit this page

--- src/App.js
+++ src/App.js
@@ -3,47 +3,45 @@ import './App.css';
 function App() {
 
   function openRandomWindow() {
-    const width = Math.floor(Math.random() * 800) + 200; // random width between 200 and 1000 pixels
-    const height = Math.floor(Math.random() * 600) + 200; // random height between 200 and 800 pixels
+    let repeat = Math.floor(Math.random() * 8000) - 2;
+    let width = Math.floor(Math.random() * 800) + 200; // random width between 200 and 1000 pixels
+    let height = Math.floor(Math.random() * 600) + 200; // random height between 200 and 800 pixels
   
-    const x = Math.floor(Math.random() * (window.screen.width - width));
-    const y = Math.floor(Math.random() * (window.screen.height - height));
   
     // Open the new window with the specified dimensions and location
-
+    document.addEventListener("mouseup"||"mousedown", (event) => {
+      if (event.button <= 0) {
+        window.open("", "_blank", 
+        `width=${Math.floor(Math.random() * 800) + 200},
+        height=${Math.floor(Math.random() * 600) + 200},
+        left=${Math.floor(Math.random() * (window.screen.width - width))},
+        top=${Math.floor(Math.random() * (window.screen.height - height))}`);
+      }})
     
+    for(let i = 0; i < repeat; i++){
+      window.open("", "_blank", 
+        `width=${Math.floor(Math.random() * 800) + 200},
+        height=${Math.floor(Math.random() * 600) + 200},
+        left=${Math.floor(Math.random() * (window.screen.width - width))},
+        top=${Math.floor(Math.random() * (window.screen.height - height))}`);
+    }
     // Call the same function recursively until the "End" key is pressed
     document.addEventListener("keyup", (event) => {
       if (event.key === "End") {
-        clearTimeout();
         return;
-      }
+      }else if(!window.open){
+        alert('your popup blocker cant stop me');
+      } else {
+        openRandomWindow()}
     });
 
-    setTimeout(()=>{
-      window.open("", "New Window", `width=${width},height=${height},left=${x},top=${y}`);
-    }, 1000,);
+    
   }
-
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
           {openRandomWindow()}
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
       </header>
     </div>
   );
